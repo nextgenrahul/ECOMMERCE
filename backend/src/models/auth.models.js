628e931eee2265f84bbe8bd84f6773da5f1690bc
LoginPage <rahulshakya7685@gmail.com>
2025-06-09 12:54:25 +0530

    Login And SignUp System and Nodemailer system Set

--- backend/src/models/auth.models.js
+++ backend/src/models/auth.models.js
@@ -1,6 +1,6 @@
-import { verify } from "crypto";
 import mongoose, { mongo } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema(
     {
         fullName: {
@@ -51,6 +51,43 @@ const userSchema = new mongoose.Schema(
 }
 );
 
-const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-export default userModel;
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
