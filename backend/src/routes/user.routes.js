bb85f0e7e84ba09a202da1803ed44d256f7703e7
LoginPage <rahulshakya7685@gmail.com>
2025-06-20 09:21:04 +0530

    Hide something

--- backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUserData } from "../controllers/user.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-
-const userRouter = express.Router();
-
-userRouter.get('/data', verifyJWT, getUserData);
-
-export default userRouter;
