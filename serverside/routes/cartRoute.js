62e3e0103791847d4e38bdc456cdf39c711c2bf1
LoginPage <rahulshakya7685@gmail.com>
2025-07-15 23:06:01 +0530

    refactor: Remove unnecessary whitespace in cartRoute.js

--- serverside/routes/cartRoute.js
+++ serverside/routes/cartRoute.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js';
 import authUser from '../middleware/auth.js';
-
 const cartRouter = express.Router();
-
 cartRouter.get('/get', authUser, getUserCart);
 cartRouter.post('/add', authUser, addToCart);
 cartRouter.post('/update', authUser, updateCart);
