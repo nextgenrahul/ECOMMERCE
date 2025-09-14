32a10380795a74031908a3f81574a75b01a30414
Rahul Shakya <rahulshakya7685@gmail.com>
2025-09-14 18:29:22 +0530

    sss

--- serverside/server.js
+++ serverside/server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 const whiteList = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://localhost:5175"
+  "http://localhost:5175",
+  "https://ecommerce-frontend-black-kappa.vercel.app"
 
 ];
 app.use(
