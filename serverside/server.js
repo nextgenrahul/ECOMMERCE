6355d7b578ac1d942d16bdd5bb4ded243a5a9b20
Rahul Shakya <rahulshakya7685@gmail.com>
2025-09-14 15:57:04 +0530

    Some change

--- serverside/server.js
+++ serverside/server.js
@@ -17,8 +17,10 @@ const whiteList = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://ecommerce-gqp2.onrender.com",
-  "https://ecommerce-1-qxo4.onrender.com"
+  "https://ecommerce-frontend-sigma-two-40.vercel.app",
+  "https://ecommerce-admin-three-lovat.vercel.app/"
+  // "https://ecommerce-gqp2.onrender.com",
+  // "https://ecommerce-1-qxo4.onrender.com"
 
 ];
 app.use(
