c036a14c7863766fd41c23a765172af7d9ea9e48
Rahul Shakya <rahulshakya7685@gmail.com>
2026-07-01 21:08:49 +0530

    MainHome component; improve structure and layout for enhanced readability and user experience

--- frontend/src/pages/MainHome.jsx
+++ frontend/src/pages/MainHome.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import Title from "../components/Title";
-import OurPolicy from "../components/OurPolicy";
-import NewsLetterBox from "../components/NewsLetterBox";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const MainHome = () => {
-  
   return (
-    <div>
+    <main className="w-full bg-white text-black overflow-x-hidden space-y-4 md:space-y-8 animate-fade-in">
+      
       <Hero />
+      
       <LatestCollection />
+      
       <BestSeller />
+      
       <OurPolicy />
+      
       <NewsLetterBox />
-    </div>
+      
+    </main>
   );
 };
 
